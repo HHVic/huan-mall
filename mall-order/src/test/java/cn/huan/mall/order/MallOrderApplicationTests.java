f776c36954755a124c199369c0b1324bf3e8513b
HHVic <182070022@stu.just.edu.cn>
2020-09-14 13:24:39 +0800

    initial project

---
+++ mall-order/src/test/java/cn/huan/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.huan.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
